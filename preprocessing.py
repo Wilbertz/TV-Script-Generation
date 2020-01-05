5eec850477a0040e2e699afe31e7d1e377df0720
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2020-01-05 21:43:06 +0100

    First version of create_lookup_tables.

--- preprocessing.py
+++ preprocessing.py
@@ -7,9 +7,14 @@ def create_lookup_tables(text):
     :param text: The text of tv scripts split into words
     :return: A tuple of dicts (vocab_to_int, int_to_vocab)
     """
-
     vocab_to_int = {}
     int_to_vocab = {}
+
+    words = set(text)
+
+    for index, word in enumerate(words):
+        vocab_to_int[word] = index
+        int_to_vocab[index] = word
 
     # return tuple
     return vocab_to_int, int_to_vocab
