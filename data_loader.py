ae4312b0a9fef18938b8142c9c446ce3a2099c27
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2020-01-06 09:22:28 +0100

    Initial version data loader.

---
+++ data_loader.py
@@ -0,0 +1,15 @@
+from torch.utils.data import TensorDataset, DataLoader
+
+
+def batch_data(words, sequence_length, batch_size):
+    """
+    Batch the neural network data using DataLoader
+    :param words: The word ids of the TV scripts
+    :param sequence_length: The sequence length of each batch
+    :param batch_size: The size of each batch; the number of sequences in a batch
+    :return: DataLoader with batched data
+    """
+    # TODO: Implement function
+
+    # return a data loader
+    return None
