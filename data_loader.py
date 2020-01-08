ae50e83c2e684432591ac74093698264e62decd5
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2020-01-08 18:32:02 +0100

    First working version of data loader.

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,5 @@
+import numpy as np
+import torch
 from torch.utils.data import TensorDataset, DataLoader
 
 
@@ -9,7 +11,37 @@ def batch_data(words, sequence_length, batch_size):
     :param batch_size: The size of each batch; the number of sequences in a batch
     :return: DataLoader with batched data
     """
-    # TODO: Implement function
+    # Get the number of batches we can make
+    n_batches = len(words) // batch_size
+    # Keep only enough characters to make full batches
+    words = words[:n_batches * batch_size]
+    
+    x, y = [], []
+    
+    for idx_start in range(0, len(words) - sequence_length):
+        idx_end = sequence_length + idx_start
+
+        x_batch = words[idx_start:idx_end]
+        y_batch = words[idx_end]
+
+        x.append(x_batch)
+        y.append(y_batch)
+
+    data = TensorDataset(torch.from_numpy(np.asarray(x)), torch.from_numpy(np.asarray(y)))
+    data_loader = DataLoader(data, shuffle=True, batch_size=batch_size)
 
     # return a data loader
-    return None
+    return data_loader
+
+
+test_text = range(50)
+
+t_loader = batch_data(test_text, sequence_length=5, batch_size=10)
+data_iter = iter(t_loader)
+sample_x, sample_y = data_iter.next()
+
+print(sample_x.shape)
+print(sample_x)
+print()
+print(sample_y.shape)
+print(sample_y)
